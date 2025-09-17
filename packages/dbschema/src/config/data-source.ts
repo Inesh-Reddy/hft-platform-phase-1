8a5ced13d2d317ffa881c4599b5761676606b78f
Inesh-Reddy <ineshreddy007@gmail.com>
2025-09-17 08:53:38 +0530

    added Entities for market, Orderbook, trade

---
+++ packages/dbschema/src/config/data-source.ts
@@ -0,0 +1,12 @@
+import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  url: `postgres://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DOCKER_DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+  migrations: [__dirname + "/../migrations/*.{ts,js}"],
+  entities: [__dirname + "/../entities/*.{ts,js}"],
+  synchronize: false,
+});
