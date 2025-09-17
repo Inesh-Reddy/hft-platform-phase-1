d89ee90897db9a6f7f79845fc42abc8a85a882d2
Inesh-Reddy <ineshreddy007@gmail.com>
2025-09-17 10:37:14 +0530

    generated migrations

--- packages/dbschema/src/config/data-source.ts
+++ packages/dbschema/src/config/data-source.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-
+console.log(process.env.DATABASE_HOST);
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: `postgres://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DOCKER_DATABASE_PORT}/${process.env.DATABASE_NAME}`,
