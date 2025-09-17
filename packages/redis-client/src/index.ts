1d8a806123461f5d72c000c9c4d688e748902d58
Inesh-Reddy <ineshreddy007@gmail.com>
2025-09-17 13:12:28 +0530

    redis client initialization

--- packages/redis-client/src/index.ts
+++ packages/redis-client/src/index.ts
@@ -3,8 +3,6 @@ import { createClient, RedisClientType } from "redis";
 export const redisClient: RedisClientType = createClient({
   url: process.env.REDIS_URL ?? "redis://localhost:6379",
 });
-
-// connect once when this package is imported
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
 redisClient.connect();
 
